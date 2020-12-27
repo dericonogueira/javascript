49cec7e68b7c2c0deeb23adb881a2e65a983f9f6
dericonogueira <76665822+dericonogueira@users.noreply.github.com>
2020-12-27 18:28:33 -0300

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,15 @@
+var modalBtn = document.querySelector('.modal-btn');
+var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.closeX')
+
+
+
+modalBtn.addEventListener('click',function(){
+    modalBg.classList.add('bg-active');
+});
+
+
+modalClose.addEventListener('click',function(){
+
+    modalBg.classList.remove('bg-active');
+})
